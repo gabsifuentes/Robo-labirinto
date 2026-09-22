24dafd085394f400775386338ff403a2fba405e9
gabsifuentes <gabsifuentesss@gmail.com>
2026-09-22 11:12:20 -0400

    definição dos pinos

--- Robo.cpp/Navegacao.cpp
+++ Robo.cpp/Navegacao.cpp
@@ -1,5 +1,20 @@
 #include <Servo.h>
 
+//PINOS SENSOR E SERVO
+#define TRIG 10
+#define ECHO 11
+#define SERVO 9
+
+// Motor a
+#define in1 2
+#define in2 3
+// Motor b
+#define in3 4
+#define in4 7
+// pinos enables (vel dos motores)
+#define enA 3
+#define enB 6
+
 Servo meuServo;
 
 class Robo {
